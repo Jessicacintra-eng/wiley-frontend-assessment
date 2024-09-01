a6803cf085731333a5caeaaa4b49f315857c64b1
Jessica Cintra <jessica.cintra0712@gmail.com>
2024-09-01 13:35:04 -0300

    adjustments in the header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <img
+        className="logo"
+        src="src/assets/wiley-logo-white.svg"
+        alt="Wiley logo"
+        loading="lazy"
+      />
+    </div>
+  )
+}
